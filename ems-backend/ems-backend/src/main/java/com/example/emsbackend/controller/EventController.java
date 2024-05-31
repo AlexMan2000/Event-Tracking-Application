a6e00f167e3a70d8f885b4c00d0021d302da5d1c
AlexMan <niallni1980@gmail.com>
2024-05-30 21:28:18 -0400

    20240530

--- ems-backend/ems-backend/src/main/java/com/example/emsbackend/controller/EventController.java
+++ ems-backend/ems-backend/src/main/java/com/example/emsbackend/controller/EventController.java
@@ -4,10 +4,8 @@ package com.example.emsbackend.controller;
 import com.example.emsbackend.dto.secondary.EventEntityDTO;
 import com.example.emsbackend.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
 
 @RestController
